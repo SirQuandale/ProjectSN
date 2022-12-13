1c22af37df2f1f8e6730fd003c94c07508649f0d
SirQuandale <302685723@student.rocmondriaan.nl>
2022-12-13 10:34:41 +0100

    article gradient & more

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 function readMoreFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
-    var btnText = document.getElementById("rMbt");
-  
+    var btnText = document.querySelector('.rmt');
+
     if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
     }
-  }
+}
+
+let text = '<div class="pwr">' + '<article>' + '<h3>Template</h3>' + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</p>' + '<p class="rmt">Read More</p>' + '</article>' + '<br>' + '</div>';
+
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
+document.querySelector('.list').innerHTML += text;
