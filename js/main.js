210037eec6dd3c7976486316aee26e6583cd86cb
SirQuandale <302685723@student.rocmondriaan.nl>
2022-12-13 12:28:19 +0100

    like and dislike html

--- js/main.js
+++ js/main.js
@@ -14,7 +14,8 @@ function readMoreFunction() {
     }
 }
 
-let text = '<div class="pwr">' + '<article>' + '<h3>Template</h3>' + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</p>' + '<p class="rmt">Read More</p>' + '</article>' + '<br>' + '</div>';
+let postTxt = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</p>'
+let text = '<div class="pwr">' + '<article>' + ' <div class="sideArtc">' + '<img src="img/1946428.png" alt="Account Settings" class="pfp">'+ '<img src="img/99021d.png" alt="Like" class="like">' + '<p class="likeCount">99</p>' + '<img src="img/99022d.png" alt="dislike" class="dislike">' + '<p class="dislikeCount">99</p>' +'</div>' + '<h3>Template</h3>' + postTxt + '<p class="rmt">Read More</p>' + '</article>' + '<br>' + '</div>';
 
 document.querySelector('.list').innerHTML += text;
 document.querySelector('.list').innerHTML += text;
